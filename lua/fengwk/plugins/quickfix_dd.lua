f04534aa4d4d2b3812c4aef6ec4843afc46619e1
fengwk <759543714@qq.com>
2023-09-27 14:09:46 +0800

    quickfix dd

---
+++ lua/fengwk/plugins/quickfix_dd.lua
@@ -0,0 +1,16 @@
+-- https://stackoverflow.com/questions/42905008/quickfix-list-how-to-add-and-remove-entries
+-- 使用dd移除quickfix列表中的项
+vim.cmd[[
+" When using `dd` in the quickfix list, remove the item from the quickfix list.
+function! RemoveQFItem()
+  let curqfidx = line('.') - 1
+  let qfall = getqflist()
+  call remove(qfall, curqfidx)
+  call setqflist(qfall, 'r')
+  execute curqfidx + 1 . "cfirst"
+  :copen
+endfunction
+:command! RemoveQFItem :call RemoveQFItem()
+" Use map <buffer> to only map dd in the quickfix window. Requires +localmap
+autocmd FileType qf map <buffer> dd :RemoveQFItem<cr>
+]]
