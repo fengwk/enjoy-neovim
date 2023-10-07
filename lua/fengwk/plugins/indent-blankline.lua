2f079a61ca92c8656d5d06cfc1bf71cf1ed4a791
fengwk <759543714@qq.com>
2023-10-07 13:30:41 +0800

    update

--- lua/fengwk/plugins/indent-blankline.lua
+++ lua/fengwk/plugins/indent-blankline.lua
@@ -1,12 +1,12 @@
 -- https://github.com/lukas-reineke/indent-blankline.nvim
 
-local ok, indent_blankline = pcall(require, "indent_blankline")
+local ok, ibl = pcall(require, "ibl")
 if not ok then
   vim.notify("indent_blankline can not be required.")
   return
 end
 
-indent_blankline.setup({
+ibl.setup({
   -- 需要treesiter支持，开启将调用treesitter影响大文件的启动
   -- show_current_context = true, -- 如果为true突出当前区域缩进线
   -- show_current_context_start = false, -- 如果为true突出当前区域开始位置
