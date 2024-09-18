7a3c9698615b98a10efedd1f3ebe796a620b99de
fengwk <759543714@qq.com>
2024-09-19 00:47:26 +0800

    fix codeaction

--- lua/telescope/_extensions/my-ui-select.lua
+++ lua/telescope/_extensions/my-ui-select.lua
@@ -1,3 +1,4 @@
+-- https://github.com/nvim-telescope/telescope-ui-select.nvim
 local ext_common = require("telescope._extensions.common")
 return require("telescope").register_extension {
   setup = function(topts)
@@ -29,11 +30,21 @@ return require("telescope").register_extension {
               client_name = 0,
             }
             for idx, item in ipairs(items) do
-              local client = vim.lsp.get_client_by_id(item[1])
+              local client_id, title
+              if vim.version and vim.version.cmp(vim.version(), vim.version.parse "0.10-dev") >= 0 then
+                client_id = item.ctx.client_id
+                title = item.action.title
+              else
+                client_id = item[1]
+                title = item[2].title
+              end
+
+              local client = vim.lsp.get_client_by_id(client_id)
+
               local entry = {
                 idx = idx,
                 ["add"] = {
-                  command_title = item[2].title:gsub("\r\n", "\\r\\n"):gsub("\n", "\\n"),
+                  command_title = title:gsub("\r\n", "\\r\\n"):gsub("\n", "\\n"),
                   client_name = client and client.name or "",
                 },
                 text = item,
@@ -129,3 +140,4 @@ return require("telescope").register_extension {
     end
   end,
 }
+
