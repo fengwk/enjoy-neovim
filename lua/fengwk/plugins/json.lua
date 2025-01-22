1138f477d59bd2fdc85aa8121d0f847a2130d727
fengwk <759543714@qq.com>
2025-01-22 17:43:49 +0800

    update

--- lua/fengwk/plugins/json.lua
+++ lua/fengwk/plugins/json.lua
@@ -8,7 +8,7 @@ local mode_range_map = {
 }
 local function gen_format_json_cmd(mode)
   local shiftwidth = vim.api.nvim_buf_get_option(0, "shiftwidth") or 4
-  return mode_range_map[mode] .. "!python " .. format_json .. " -i " .. shiftwidth
+  return mode_range_map[mode] .. "!python3 " .. format_json .. " -i " .. shiftwidth
 end
 
 vim.api.nvim_create_augroup("user_json", { clear = true })
